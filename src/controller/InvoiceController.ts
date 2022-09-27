7b4b7a4740bfc5c32b4f95d82cc598c74d3f48c6
Jonas-D-M <43569205+Jonas-D-M@users.noreply.github.com>
2022-09-27 15:28:56 +0200

    feat: invoice and controller

---
+++ src/controller/InvoiceController.ts
@@ -0,0 +1,84 @@
+import { NextFunction, Request, Response } from 'express'
+import { In } from 'typeorm'
+import { AppDataSource } from '../data-source'
+import { Invoice } from '../entity/Invoice'
+import { Order } from '../entity/Order'
+import { OrderProduct } from '../entity/OrderProduct'
+import { Product } from '../entity/Product'
+
+export class InvoiceController {
+  private invoiceRepository = AppDataSource.getRepository(Invoice)
+  private manager = AppDataSource.manager
+
+  async all(request: Request, response: Response, next: NextFunction) {
+    try {
+      return this.invoiceRepository.find()
+    } catch (error) {
+      console.error(error)
+      response.statusCode = 500
+      response.send()
+    }
+  }
+
+  async one(request: Request, response: Response, next: NextFunction) {
+    try {
+      const invoice = await this.invoiceRepository.findOne({
+        where: { id: parseInt(request.params.id, 10) },
+      })
+
+      if (invoice) {
+        return invoice
+      }
+      response.statusCode = 404
+      response.statusMessage = 'Not found'
+      response.send()
+    } catch (error) {
+      console.error(error)
+      response.statusCode = 500
+      response.send()
+    }
+  }
+
+  async save(request: Request, response: Response, next: NextFunction) {
+    try {
+      const invoice = new Invoice()
+
+      invoice.customer = request.body.customer
+      invoice.orders = await this.manager.findBy(Order, {
+        id: In(request.body.orders),
+      })
+
+      return this.invoiceRepository.save(invoice)
+    } catch (error) {
+      console.error(error)
+      response.statusCode = 500
+      response.send()
+    }
+  }
+
+  async update(request: Request, response: Response) {
+    try {
+      const invoice = await this.invoiceRepository.preload(request.body)
+
+      await this.invoiceRepository.save(invoice)
+      return this.invoiceRepository.findOneBy({ id: request.body.id })
+    } catch (error) {
+      console.error(error)
+      response.statusCode = 500
+      response.send()
+    }
+  }
+
+  async remove(request: Request, response: Response) {
+    try {
+      let invoiceToRemove = await this.invoiceRepository.findOneBy({
+        id: parseInt(request.params.id, 10),
+      })
+      return await this.invoiceRepository.remove(invoiceToRemove)
+    } catch (error) {
+      console.error(error)
+      response.statusCode = 500
+      response.send()
+    }
+  }
+}
